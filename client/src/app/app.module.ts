9e1d7f52ff0352c7bbe880d564258caa1fe7cd4f
William Balea <williambalea27@gmail.com>
2019-09-09 23:53:00 -0400

    Added entry-point component to app-root [WB] (D)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './components/app/app.component';
+import { EntryPointComponent } from './entry-point/entry-point.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    EntryPointComponent,
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/entry-point/entry-point.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-point',
+  templateUrl: './entry-point.component.html',
+  styleUrls: ['./entry-point.component.scss']
+})
+export class EntryPointComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

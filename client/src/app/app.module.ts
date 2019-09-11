110c248077268445b5fdf5bec040ab8e9abb09f5
ines <ines.goddi@polymtl.ca>
2019-09-11 19:31:12 -0400

    adding lateral side bar [AB][IG] (D)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './components/app/app.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SideBarComponent,
   ],
   imports: [
     BrowserModule,

affa5aa61f4fcc815cc1ce0d62571cae56f51b4e
ines <ines.goddi@polymtl.ca>
2019-09-11 19:59:32 -0400

    adding lateral attribute bar [AB][IG](D)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './components/app/app.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { AttributeBarComponent } from './components/attribute-bar/attribute-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
+    AttributeBarComponent,
   ],
   imports: [
     BrowserModule,

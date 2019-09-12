89c904af28bc9d0fb01b1443514769435f6e7287
ines <ines.goddi@polymtl.ca>
2019-09-12 16:26:47 -0400

    changing lateral bar implementation to using angular material toolbar and adding icons to it [IG](D)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,32 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './components/app/app.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { AttributeBarComponent } from './components/attribute-bar/attribute-bar.component';
+import { BackgroundComponent } from './components/background/background.component';
+import { DrawingSpaceComponent } from './components/drawing-space/drawing-space.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from  '@angular/material';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
     AttributeBarComponent,
+    BackgroundComponent,
+    DrawingSpaceComponent,
+    
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    AngularSvgIconModule
   ],
   providers: [],
   bootstrap: [AppComponent],

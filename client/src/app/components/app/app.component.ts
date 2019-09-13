21a31b7a6ab25b02116c912beb1fcbbba2201f74
William Balea <williambalea27@gmail.com>
2019-09-13 12:09:15 -0400

    Temporary fix for svg height [WB] (D)

--- client/src/app/components/app/app.component.ts
+++ client/src/app/components/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent implements OnInit {
   public enableKeyPress: boolean = false;
 
   public canvasWidth: number = window.innerWidth;
-  public canvasHeigth: number = window.innerHeight;
+  public canvasHeigth: number = 1080;
   public rectangles: rect[] = [];
 
   // TODO: interface mouse and style to reduce parameters count
@@ -38,11 +38,15 @@ export class AppComponent implements OnInit {
   public previewStroke: string;
   public previewStrokeWidth: number;
 
+  public shiftPressed: boolean;
+
   public constructor(private dialog: MatDialog) { }
 
   public ngOnInit(): void {
     if(!sessionStorage.getItem("hideDialog"))
       this.openDialog();
+    else
+      this.enableKeyPress = true;
   }
 
 
@@ -59,55 +63,81 @@ export class AppComponent implements OnInit {
   }
 
   @HostListener("window:keydown", ["$event"])
-  public onKeyDown($event: KeyboardEvent): void {
+  public onKeyDown(event: KeyboardEvent): void {
     if(this.enableKeyPress) {
+      if(event.key === "Shift") {
+        this.previewWidth = this.previewHeight;
+        this.shiftPressed = true;
+      }
+      if(event.ctrlKey && event.key === 'z') {
+        this.rectangles.pop();
+        console.log("pressed z rectangles.length : " + this.rectangles.length );
+      }
     }
   }
 
-  public setRGBColor(r: number, g: number, b: number): string {
-    return `rgb(${r},${g},${b})`;
+  /*
+  @HostListener("window:keyup", ["$event"])
+  public onKeyUp($event: KeyboardEvent): void {
+    console.log("shift up");
+    if($event.key === "Shift")
+      this.shiftPressed = false;
+  }
+  */
+
+  public setRGBAColor(r: number, g: number, b: number, a: number): string {
+    return `rgb(${r},${g},${b},${a})`;
   }
 
   public REMOVEsetRectStyle(): void {
     const r: number = Math.floor(Math.random() * 255);
     const g: number = Math.floor(Math.random() * 255);
     const b: number = Math.floor(Math.random() * 255);
-    this.previewFill = this.setRGBColor(r, g, b);
-    this.previewStrokeWidth = 1;
-    this.previewStroke = this.setRGBColor(0,0, 0);
+    const a: number = Math.round(Math.random());
+    this.previewFill = this.setRGBAColor(r, g, b, 1);
+    this.previewStrokeWidth = 2;
+    this.previewStroke = this.setRGBAColor(0,0, 0, a);
   }
 
   @HostListener("mousedown", ["$event"])
-  public setMouseInitalCoord($event: MouseEvent): void {
+  public setMouseInitalCoord(event: MouseEvent): void {
     this.REMOVEsetRectStyle();
     this.previewActive = true;
-    this.previewX = $event.offsetX;
-    this.previewY = $event.offsetY;
+    this.previewX = event.offsetX;
+    this.previewY = event.offsetY;
 
-    this.mouseInitialX = $event.offsetX;
-    this.mouseInitialY = $event.offsetY;
+    this.mouseInitialX = event.offsetX;
+    this.mouseInitialY = event.offsetY;
   }
 
   @HostListener("mousemove", ["$event"])
-  public setPreviewOffset($event: MouseEvent): void {
+  public setPreviewOffset(event: MouseEvent): void {
     if(this.previewActive) {
-      this.previewWidth = Math.abs($event.offsetX - this.mouseInitialX);
-      this.previewHeight = Math.abs($event.offsetY - this.mouseInitialY);
-      this.previewX = this.mouseInitialX < $event.offsetX ? this.mouseInitialX : $event.offsetX;
-      this.previewY = this.mouseInitialY < $event.offsetY ? this.mouseInitialY : $event.offsetY;
+      this.previewWidth = Math.abs(event.offsetX - this.mouseInitialX);
+      if(this.shiftPressed) {
+        this.previewHeight = this.previewWidth;
+        this.previewX = event.offsetX > 0 ? this.mouseInitialX : -this.mouseInitialX;
+        this.previewY = event.offsetY > 0 ? this.mouseInitialY : -this.mouseInitialY;
+      } else {
+        this.previewHeight = Math.abs(event.offsetY - this.mouseInitialY);
+        this.previewX = this.mouseInitialX < event.offsetX ? this.mouseInitialX : event.offsetX;
+        this.previewY = this.mouseInitialY < event.offsetY ? this.mouseInitialY : event.offsetY;
+      }
     }
   }
 
-  @HostListener("mouseup", ["$event"])
-  public drawSquare($event: MouseEvent): void {
-    const rectangle: rect = { x: this.previewX, 
-                              y: this.previewY, 
-                              width: this.previewWidth, 
-                              height: this.previewHeight,
-                              fill: this.previewFill,
-                              strokeWidth: this.previewStrokeWidth,
-                              stroke: this.previewStroke };
-                              
+  @HostListener("mouseup")
+  public drawSquare(): void {
+    const rectangle: rect = { 
+      x: this.previewX, 
+      y: this.previewY, 
+      width: this.previewWidth, 
+      height: this.previewHeight,
+      fill: this.previewFill,
+      strokeWidth: this.previewStrokeWidth,
+      stroke: this.previewStroke 
+    };
+
     this.rectangles.push(rectangle);
     this.previewActive = false;
     this.previewWidth = 0;

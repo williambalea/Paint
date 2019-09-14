19a55b8ef190708b7e7b90a7cc9a73b7504c02f2
William Balea <williambalea27@gmail.com>
2019-09-14 14:19:13 -0400

    Added setSquareOffset() and hostListener on shift presses [WB] (D)

--- client/src/app/components/drawing-space/drawing-space.component.ts
+++ client/src/app/components/drawing-space/drawing-space.component.ts
@@ -1,9 +1,9 @@
 import { Component,  HostListener, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Mouse } from '../../../../../common/interface/mouse';
 import { Preview } from '../../../../../common/interface/preview';
 import { ShapesService } from '../../services/shapes/shapes.service';
 import { EntryPointComponent } from '../entry-point/entry-point.component';
-import { Mouse } from '../../../../../common/interface/mouse';
 
 @Component({
   selector: 'app-drawing-space',
@@ -12,7 +12,7 @@ import { Mouse } from '../../../../../common/interface/mouse';
 })
 export class DrawingSpaceComponent implements OnInit {
   canvasWidth: number = window.innerWidth;
-  canvasHeigth = 1080;
+  canvasHeight: number;
 
   enableKeyPress: boolean;
   shiftPressed: boolean;
@@ -23,11 +23,12 @@ export class DrawingSpaceComponent implements OnInit {
 
   previewActive = false;
   preview: Preview;
-
-  origin: Mouse = {x: 0, y: 0};
+  origin: Mouse;
 
   constructor(private dialog: MatDialog,
               private shapeService: ShapesService) {
+    this.canvasWidth = window.innerWidth;
+    this.canvasHeight = window.innerHeight;
     this.enableKeyPress = false;
 
   }
@@ -44,18 +45,31 @@ export class DrawingSpaceComponent implements OnInit {
     const dialogRef: MatDialogRef<EntryPointComponent, any> =
       this.dialog.open(EntryPointComponent, { disableClose: true });
 
-    dialogRef.afterClosed().subscribe((hideDialog: boolean) => {
-      if (hideDialog) {
-        sessionStorage.setItem('hideDialog', JSON.stringify(hideDialog));
-      }
-      this.enableKeyPress = true;
-    });
+    dialogRef.afterClosed()
+    .subscribe((hideDialog: boolean) => { this.closeDialog(hideDialog); });
+  }
 
+  closeDialog(hideDialog: boolean): void {
+    if (hideDialog) {
+      sessionStorage.setItem('hideDialog', JSON.stringify(hideDialog));
+    }
+    this.enableKeyPress = true;
   }
 
   @HostListener('window:keydown', ['$event'])
   onKeyDown(event: KeyboardEvent): void {
-    if (this.enableKeyPress) { return; }
+    if (this.enableKeyPress) {
+      if (event.key === 'Shift') {
+        this.shiftPressed = true;
+      }
+    }
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  onKeyUp(event: KeyboardEvent): void {
+    if (event.key === 'Shift') {
+      this.shiftPressed = false;
+    }
   }
 
   // TODO: to be put elswhere
@@ -74,47 +88,46 @@ export class DrawingSpaceComponent implements OnInit {
   }
 
   @HostListener('mousedown', ['$event'])
-  setMouseInitalCoord(event: MouseEvent): void {
+  setMouseOrigin(event: MouseEvent): void {
     this.REMOVEsetRectStyle();
 
     this.preview = {
-      x: 0,
-      y: 0,
+      x: event.offsetX,
+      y: event.offsetY,
       width: 0,
-      height: 0,
-    };
+      height: 0 };
+
+    this.origin = {x: event.offsetX, y: event.offsetY};
 
     this.previewActive = true;
-
-    this.preview.x = event.offsetX;
-    this.preview.y = event.offsetY;
-
-    this.origin.x = event.offsetX;
-    this.origin.y = event.offsetY;
   }
 
   @HostListener('mousemove', ['$event'])
   setPreviewOffset(event: MouseEvent): void {
     if (this.previewActive) {
-      this.preview.width = Math.abs(event.offsetX - this.origin.x);
       if (this.shiftPressed) {
-        this.preview.height = this.preview.width;
-        this.preview.x = event.offsetX > 0 ? this.origin.x : -this.origin.x;
-        this.preview.y = event.offsetY > 0 ? this.origin.y : -this.origin.y;
+        this.setSquareOffset(event);
       } else {
-        this.preview.height = Math.abs(event.offsetY - this.origin.y);
-        this.preview.x = this.origin.x < event.offsetX ? this.origin.x : event.offsetX;
-        this.preview.y = this.origin.y < event.offsetY ? this.origin.y : event.offsetY;
+        this.setRectangleOffset(event);
       }
     }
+  }
+
+  setRectangleOffset(event: MouseEvent): void {
+    this.preview.width = Math.abs(event.offsetX - this.origin.x);
+    this.preview.height = Math.abs(event.offsetY - this.origin.y);
+    this.preview.x = Math.min(this.origin.x, event.offsetX);
+    this.preview.y = Math.min(this.origin.y, event.offsetY);
+  }
+
+  setSquareOffset(event: MouseEvent): void {
+    return;
   }
 
   @HostListener('mouseup')
   drawShape(): void {
     this.shapeService.drawRectangle(this.preview, this.fill, this.stroke, this.strokeWidth);
     this.previewActive = false;
-    this.preview.width = 0;
-    this.preview.height = 0;
   }
 
 }

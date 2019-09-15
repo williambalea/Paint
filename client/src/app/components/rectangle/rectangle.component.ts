78b11f10b89dcbc8b73ca44392279d32aca98a4e
Omar BEN SALAH <omar.ben-salah@polymtl.ca>
2019-09-15 15:50:11 -0400

    added color picker tool [OBS] (D)

--- client/src/app/components/rectangle/rectangle.component.ts
+++ client/src/app/components/rectangle/rectangle.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-rectangle',
   templateUrl: './rectangle.component.html',
   styleUrls: ['./rectangle.component.scss'],
 })
-export class RectangleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class RectangleComponent {
 
 }

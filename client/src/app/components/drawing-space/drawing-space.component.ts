0825382e100e63ed87e1dbfc4d8e3b5ef6bb4a5e
William Balea <williambalea27@gmail.com>
2019-09-15 01:14:49 -0400

    Moved all rectangles functions from drawing-space.component to shape.service [WB] (D)

--- client/src/app/components/drawing-space/drawing-space.component.ts
+++ client/src/app/components/drawing-space/drawing-space.component.ts
@@ -1,7 +1,5 @@
 import { Component,  HostListener, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { Point } from '../../../../../common/interface/point';
-import { Preview } from '../../../../../common/interface/preview';
 import { ShapesService } from '../../services/shapes/shapes.service';
 import { EntryPointComponent } from '../entry-point/entry-point.component';
 
@@ -13,32 +11,20 @@ import { EntryPointComponent } from '../entry-point/entry-point.component';
 export class DrawingSpaceComponent implements OnInit {
   canvasWidth: number;
   canvasHeight: number;
-  offset: Point;
 
   enableKeyPress: boolean;
   shiftPressed: boolean;
 
+  // TODO: make an interface
   fill: string;
   stroke: string;
   strokeWidth: number;
-
-  preview: Preview;
-  origin: Point;
 
   constructor(private dialog: MatDialog,
               private shapeService: ShapesService) {
     this.canvasWidth = window.innerWidth;
     this.canvasHeight = window.innerHeight;
     this.enableKeyPress = false;
-
-    this.preview = {
-      active: false,
-      x: 0,
-      y: 0,
-      width: 0,
-      height: 0,
-    };
-
   }
 
   ngOnInit(): void {
@@ -47,6 +33,48 @@ export class DrawingSpaceComponent implements OnInit {
     } else {
       this.enableKeyPress = true;
     }
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent): void {
+    if (this.enableKeyPress) {
+      if (event.key === 'Shift') {
+        this.shiftPressed = true;
+        this.shapeService.setSquareOffset();
+      }
+    }
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  onKeyUp(event: KeyboardEvent): void {
+    if (event.key === 'Shift') {
+      this.shiftPressed = false;
+      this.shapeService.setRectangleOffset();
+    }
+  }
+
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(event: MouseEvent): void {
+    this.TEMPORARYsetRectStyle();
+    this.shapeService.setMouseOrigin(event);
+  }
+
+  @HostListener('mousemove', ['$event'])
+  setPreviewOffset(event: MouseEvent): void {
+    this.shapeService.mouse = {x: event.offsetX, y: event.offsetY};
+    if (this.shapeService.preview.active) {
+      if (this.shiftPressed) {
+        this.shapeService.setSquareOffset();
+      } else {
+        this.shapeService.setRectangleOffset();
+      }
+    }
+  }
+
+  @HostListener('mouseup')
+  drawShape(): void {
+    this.shapeService.drawRectangle(this.shapeService.preview, this.fill, this.stroke, this.strokeWidth);
+    this.shapeService.preview.active = false;
   }
 
   openDialog(): void {
@@ -64,90 +92,19 @@ export class DrawingSpaceComponent implements OnInit {
     this.enableKeyPress = true;
   }
 
-  @HostListener('window:keydown', ['$event'])
-  onKeyDown(event: KeyboardEvent): void {
-    if (this.enableKeyPress) {
-      if (event.key === 'Shift') {
-        this.shiftPressed = true;
-        this.setSquareOffset();
-      }
-    }
-  }
-
-  @HostListener('window:keyup', ['$event'])
-  onKeyUp(event: KeyboardEvent): void {
-    if (event.key === 'Shift') {
-      this.shiftPressed = false;
-      this.setRectangleOffset();
-    }
-  }
-
-  // TODO: to be put elswhere
-  setRGBAColor(r: number, g: number, b: number, a: number): string {
-    return `rgb(${r},${g},${b},${a})`;
-  }
-
-  REMOVEsetRectStyle(): void {
+  TEMPORARYsetRectStyle(): void {
     const r: number = Math.floor(Math.random() * 255);
     const g: number = Math.floor(Math.random() * 255);
     const b: number = Math.floor(Math.random() * 255);
     const a: number = Math.round(Math.random());
-    this.fill = this.setRGBAColor(r, g, b, 1);
+    this.fill = this.TEMPORARYsetRGBAColor(r, g, b, 1);
     this.strokeWidth = 2;
-    this.stroke = this.setRGBAColor(0, 0, 0, a);
+    this.stroke = this.TEMPORARYsetRGBAColor(0, 0, 0, a);
   }
 
-  @HostListener('mousedown', ['$event'])
-  setMouseOrigin(event: MouseEvent): void {
-    this.REMOVEsetRectStyle();
-    this.origin = {x: event.offsetX, y: event.offsetY};
-    this.preview.active = true;
-    this.preview.x = event.offsetX;
-    this.preview.y = event.offsetY;
-  }
-
-  @HostListener('mousemove', ['$event'])
-  setPreviewOffset(event: MouseEvent): void {
-    this.offset = {x: event.offsetX, y: event.offsetY};
-    if (this.preview.active) {
-      if (this.shiftPressed) {
-        this.setSquareOffset();
-      } else {
-        this.setRectangleOffset();
-      }
-    }
-  }
-
-  setRectangleOffset(): void {
-    this.preview.width = Math.abs(this.offset.x - this.origin.x);
-    this.preview.height = Math.abs(this.offset.y - this.origin.y);
-    this.preview.x = Math.min(this.origin.x, this.offset.x);
-    this.preview.y = Math.min(this.origin.y, this.offset.y);
-  }
-
-  setSquareOffset(): void {
-    const deplacement: Point = {
-      x: this.offset.x - this.origin.x,
-      y: this.offset.y - this.origin.y,
-    };
-
-    const width = Math.min(Math.abs(deplacement.x), Math.abs(deplacement.y));
-
-    const newOffset: Point = {
-      x: this.origin.x + (Math.sign(deplacement.x) * width),
-      y: this.origin.y + (Math.sign(deplacement.y) * width),
-    };
-
-    this.preview.width = width;
-    this.preview.height = width;
-    this.preview.x = Math.min(this.origin.x, newOffset.x);
-    this.preview.y = Math.min(this.origin.y, newOffset.y);
-  }
-
-  @HostListener('mouseup')
-  drawShape(): void {
-    this.shapeService.drawRectangle(this.preview, this.fill, this.stroke, this.strokeWidth);
-    this.preview.active = false;
+  // TODO: to be put elswhere
+  TEMPORARYsetRGBAColor(r: number, g: number, b: number, a: number): string {
+    return `rgb(${r},${g},${b},${a})`;
   }
 
 }
--- client/src/app/services/shapes/shapes.service.ts
+++ client/src/app/services/shapes/shapes.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Point } from '../../../../../common/interface/point';
 import { Preview } from '../../../../../common/interface/preview';
 import { Rectangle } from './classes/rectangle';
 import { Shape } from './classes/shape';
@@ -7,10 +8,60 @@ import { Shape } from './classes/shape';
   providedIn: 'root',
 })
 export class ShapesService {
-
   shapes: Shape[] = [];
 
-  // TODO: put coloring in own interface
+  preview: Preview;
+  mouse: Point;
+  origin: Point;
+
+  constructor() {
+    this.preview = {
+      active: false,
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+    };
+  }
+
+  setMouseOrigin(event: MouseEvent): void {
+    this.origin = {x: event.offsetX, y: event.offsetY};
+    this.preview = {
+      active: true,
+      x: event.offsetX,
+      y: event.offsetY,
+      width: 0,
+      height: 0,
+    };
+  }
+
+  setRectangleOffset(): void {
+    this.preview.width = Math.abs(this.mouse.x - this.origin.x);
+    this.preview.height = Math.abs(this.mouse.y - this.origin.y);
+    this.preview.x = Math.min(this.origin.x, this.mouse.x);
+    this.preview.y = Math.min(this.origin.y, this.mouse.y);
+  }
+
+  setSquareOffset(): void {
+    const deplacement: Point = {
+      x: this.mouse.x - this.origin.x,
+      y: this.mouse.y - this.origin.y,
+    };
+
+    const width = Math.min(Math.abs(deplacement.x), Math.abs(deplacement.y));
+
+    const newOffset: Point = {
+      x: this.origin.x + (Math.sign(deplacement.x) * width),
+      y: this.origin.y + (Math.sign(deplacement.y) * width),
+    };
+
+    this.preview.width = width;
+    this.preview.height = width;
+    this.preview.x = Math.min(this.origin.x, newOffset.x);
+    this.preview.y = Math.min(this.origin.y, newOffset.y);
+  }
+
+  // TODO: put options (fill, etc.) in own interface
   drawRectangle(preview: Preview, fill: string, stroke: string, strokeWidth: number): void {
     const rectangle = new Rectangle (
       preview.x,

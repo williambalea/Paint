154c8296e8ee9defc5d5eaebc05ccade37c27f56
Unknown <j-m.lasnier@hotmail.com>
2019-09-19 21:31:42 -0400

    colorApplicator Tool service generated [JL](D)

---
+++ client/src/app/services/tools/color-applicator-tool.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ColorApplicatorToolService {
+
+  constructor() { }
+
+  changePrimaryColor(newPrimColor: string): string {
+
+    return "";
+  }
+
+}

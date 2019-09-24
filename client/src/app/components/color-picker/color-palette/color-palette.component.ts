60020dcb1bbd55ef43e7bfcc6bbfc5945201bd53
Omar BEN SALAH <omar.ben-salah@polymtl.ca>
2019-09-23 21:37:38 -0400

    deleted useless condition [OBS] (A)

--- client/src/app/components/color-picker/color-palette/color-palette.component.ts
+++ client/src/app/components/color-picker/color-palette/color-palette.component.ts
@@ -38,35 +38,34 @@ export class ColorPaletteComponent implements AfterViewInit, OnChanges {
     if (!this.ctx) {
       this.ctx = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
     }
-    if (this.ctx != null) {
-      const width = this.canvas.nativeElement.width;
-      const height = this.canvas.nativeElement.height;
 
-      this.ctx.fillStyle = this.hue || 'rgba(255,255,255,1)';
-      this.ctx.fillRect(0, 0, width, height);
+    const width = this.canvas.nativeElement.width;
+    const height = this.canvas.nativeElement.height;
 
-      const whiteGrad = this.ctx.createLinearGradient(0, 0, width, 0);
-      whiteGrad.addColorStop(0, 'rgba(255,255,255,1)');
-      whiteGrad.addColorStop(1, 'rgba(255,255,255,0)');
+    this.ctx.fillStyle = this.hue || 'rgba(255,255,255,1)';
+    this.ctx.fillRect(0, 0, width, height);
 
-      this.ctx.fillStyle = whiteGrad;
-      this.ctx.fillRect(0, 0, width, height);
+    const whiteGrad = this.ctx.createLinearGradient(0, 0, width, 0);
+    whiteGrad.addColorStop(0, 'rgba(255,255,255,1)');
+    whiteGrad.addColorStop(1, 'rgba(255,255,255,0)');
 
-      const blackGrad = this.ctx.createLinearGradient(0, 0, 0, height);
-      blackGrad.addColorStop(0, 'rgba(0,0,0,0)');
-      blackGrad.addColorStop(1, 'rgba(0,0,0,1)');
+    this.ctx.fillStyle = whiteGrad;
+    this.ctx.fillRect(0, 0, width, height);
 
-      this.ctx.fillStyle = blackGrad;
-      this.ctx.fillRect(0, 0, width, height);
+    const blackGrad = this.ctx.createLinearGradient(0, 0, 0, height);
+    blackGrad.addColorStop(0, 'rgba(0,0,0,0)');
+    blackGrad.addColorStop(1, 'rgba(0,0,0,1)');
 
-      if (this.selectedPosition) {
+    this.ctx.fillStyle = blackGrad;
+    this.ctx.fillRect(0, 0, width, height);
+
+    if (this.selectedPosition) {
         this.ctx.strokeStyle = 'white';
         this.ctx.fillStyle = 'white';
         this.ctx.beginPath();
         this.ctx.arc(this.selectedPosition.x, this.selectedPosition.y, 10, 0, 2 * Math.PI);
         this.ctx.lineWidth = 5;
         this.ctx.stroke();
-      }
     }
   }
 
--- client/src/app/components/color-picker/color-slider/color-slider.component.ts
+++ client/src/app/components/color-picker/color-slider/color-slider.component.ts
@@ -30,36 +30,34 @@ export class ColorSliderComponent implements AfterViewInit {
     if (!this.ctx) {
       this.ctx = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
     }
-    if (this.ctx != null) {
-      const width = this.canvas.nativeElement.width;
-      const height = this.canvas.nativeElement.height;
+    const width = this.canvas.nativeElement.width;
+    const height = this.canvas.nativeElement.height;
 
-      this.ctx.clearRect(0, 0, width, height);
+    this.ctx.clearRect(0, 0, width, height);
 
-      const gradient = this.ctx.createLinearGradient(0, 0, 0, height);
-      gradient.addColorStop(0, 'rgba(255, 0, 0, 1)');
-      gradient.addColorStop(0.17, 'rgba(255, 255, 0, 1)');
-      gradient.addColorStop(0.34, 'rgba(0, 255, 0, 1)');
-      gradient.addColorStop(0.51, 'rgba(0, 255, 255, 1)');
-      gradient.addColorStop(0.68, 'rgba(0, 0, 255, 1)');
-      gradient.addColorStop(0.85, 'rgba(255, 0, 255, 1)');
-      gradient.addColorStop(1, 'rgba(255, 0, 0, 1)');
+    const gradient = this.ctx.createLinearGradient(0, 0, 0, height);
+    gradient.addColorStop(0, 'rgba(255, 0, 0, 1)');
+    gradient.addColorStop(0.17, 'rgba(255, 255, 0, 1)');
+    gradient.addColorStop(0.34, 'rgba(0, 255, 0, 1)');
+    gradient.addColorStop(0.51, 'rgba(0, 255, 255, 1)');
+    gradient.addColorStop(0.68, 'rgba(0, 0, 255, 1)');
+    gradient.addColorStop(0.85, 'rgba(255, 0, 255, 1)');
+    gradient.addColorStop(1, 'rgba(255, 0, 0, 1)');
 
+    this.ctx.beginPath();
+    this.ctx.rect(0, 0, width, height);
+
+    this.ctx.fillStyle = gradient;
+    this.ctx.fill();
+    this.ctx.closePath();
+
+    if (this.selectedHeight) {
       this.ctx.beginPath();
-      this.ctx.rect(0, 0, width, height);
-
-      this.ctx.fillStyle = gradient;
-      this.ctx.fill();
+      this.ctx.strokeStyle = 'white';
+      this.ctx.lineWidth = 5;
+      this.ctx.rect(0, this.selectedHeight - 5, width, 10);
+      this.ctx.stroke();
       this.ctx.closePath();
-
-      if (this.selectedHeight) {
-        this.ctx.beginPath();
-        this.ctx.strokeStyle = 'white';
-        this.ctx.lineWidth = 5;
-        this.ctx.rect(0, this.selectedHeight - 5, width, 10);
-        this.ctx.stroke();
-        this.ctx.closePath();
-      }
     }
   }
 

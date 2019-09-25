6a8ac7d294149e865d815a43b29f96a3b8d72dfe
Unknown <j-m.lasnier@hotmail.com>
2019-09-24 22:06:42 -0400

    added tests to attributeBarComponent [JL](T)

--- client/src/app/components/attribute-bar/attribute-bar.component.spec.ts
+++ client/src/app/components/attribute-bar/attribute-bar.component.spec.ts
@@ -1,6 +1,13 @@
+import { ColorService } from './../../services/color/color.service';
+import { FormsModule } from '@angular/forms';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AttributeBarComponent } from './attribute-bar.component';
+import { ShapesService } from 'src/app/services/shapes/shapes.service';
+
+const shapesService: ShapesService = new ShapesService();
+const colorService: ColorService = new ColorService();
+const attributeBarComponent: AttributeBarComponent = new AttributeBarComponent(shapesService, colorService);
 
 describe('AttributeBarComponent', () => {
   let component: AttributeBarComponent;
@@ -9,6 +16,9 @@ describe('AttributeBarComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AttributeBarComponent ],
+      imports:[
+        FormsModule
+      ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     })
     .compileComponents();
@@ -23,4 +33,46 @@ describe('AttributeBarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should set brushState to true',() => {
+    attributeBarComponent.selectBrush();
+    expect(attributeBarComponent.brushState).toBe(true);
+  });
+
+  it('Should set brushState to fasle', () => {
+    attributeBarComponent.selectBrush();
+    attributeBarComponent.selectPen();
+    expect(attributeBarComponent.brushState).toBe(false);
+  });
+
+  it('Should set stroke width correctly',()=> {
+    let value: number = 10;
+    attributeBarComponent.assignStrokeWidth(value);
+    expect(shapesService.rectangleStrokeWidth).toBe(value);
+  });
+
+  it('Should set stroke color correctly',()=>{
+    let value:string = 'FFFFFF';
+    attributeBarComponent.assignStrokeColor(value);
+    expect(attributeBarComponent.shapeStrokeColor).toBe(value);
+  });
+
+  it('Should set FillColor correctly',() => {
+    let value: string = '111111';
+    attributeBarComponent.assignFillColor(value);
+    expect(attributeBarComponent.shapeFillColor).toBe(value);
+  } );
+
+  it('Should set BrushStrokeWidth correctly',() => {
+    let value:number = 10;
+    attributeBarComponent.assignBrushStrokeWidth(value);
+    expect(attributeBarComponent.brushStrokeWidth).toBe(value);
+  });
+
+  it('Should set PenStroleWidth correctly',() => {
+    attributeBarComponent.assignPenStrokeWidth(10);
+    expect(attributeBarComponent.penStrokeWidth).toBe(10);
+  });
+
+
 });

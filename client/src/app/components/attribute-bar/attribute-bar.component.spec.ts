1cef9c5921b2dff3c0b480979ce8c540aed675ef
Unknown <j-m.lasnier@hotmail.com>
2019-09-25 09:55:02 -0400

    removed outdated tests from attributebarcomponent [JL](T)

--- client/src/app/components/attribute-bar/attribute-bar.component.spec.ts
+++ client/src/app/components/attribute-bar/attribute-bar.component.spec.ts
@@ -35,17 +35,6 @@ describe('AttributeBarComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Should set brushState to true', () => {
-    attributeBarComponent.selectBrush();
-    expect(attributeBarComponent.brushState).toBe(true);
-  });
-
-  it('Should set brushState to fasle', () => {
-    attributeBarComponent.selectBrush();
-    attributeBarComponent.selectPen();
-    expect(attributeBarComponent.brushState).toBe(false);
-  });
-
   // it('Should enter switchcase Bordered', () => {
   //   attributeBarComponent.selectedType = 'Filled';
   //   attributeBarComponent.assignRectangleType();
@@ -57,29 +46,6 @@ describe('AttributeBarComponent', () => {
     const value = NB.Ten;
     attributeBarComponent.assignStrokeWidth(value);
     expect(shapesService.rectangleStrokeWidth).toEqual(value);
-  });
-
-  it('Should set stroke color correctly', () => {
-    const value = 'FFFFFF';
-    attributeBarComponent.assignStrokeColor(value);
-    expect(attributeBarComponent.shapeStrokeColor).toEqual(value);
-  });
-
-  it('Should set FillColor correctly', () => {
-    const value = '111111';
-    attributeBarComponent.assignFillColor(value);
-    expect(attributeBarComponent.shapeFillColor).toEqual(value);
-  } );
-
-  it('Should set BrushStrokeWidth correctly', () => {
-    const value = NB.Ten;
-    attributeBarComponent.assignBrushStrokeWidth(value);
-    expect(attributeBarComponent.brushStrokeWidth).toEqual(value);
-  });
-
-  it('Should set PenStroleWidth correctly', () => {
-    attributeBarComponent.assignPenStrokeWidth(NB.Ten);
-    expect(attributeBarComponent.penStrokeWidth).toEqual(NB.Ten);
   });
 
 });

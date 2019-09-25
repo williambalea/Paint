6085df7a4bccc6e1c3b339f0122fbecdc9a1365a
Unknown <j-m.lasnier@hotmail.com>
2019-09-24 22:39:59 -0400

    tests correction attributebarcomponent [JL](T)

--- client/src/app/components/attribute-bar/attribute-bar.component.spec.ts
+++ client/src/app/components/attribute-bar/attribute-bar.component.spec.ts
@@ -45,33 +45,41 @@ describe('AttributeBarComponent', () => {
     expect(attributeBarComponent.brushState).toBe(false);
   });
 
+  // it('Should enter switchcase Bordered', () => {
+  //   attributeBarComponent.selectedType = 'Filled';
+  //   attributeBarComponent.assignRectangleType();
+  //   expect(shapesService.fillEnable).toBe(true);
+
+  // })
+
+
   it('Should set stroke width correctly',()=> {
     let value: number = 10;
     attributeBarComponent.assignStrokeWidth(value);
-    expect(shapesService.rectangleStrokeWidth).toBe(value);
+    expect(shapesService.rectangleStrokeWidth).toEqual(value);
   });
 
   it('Should set stroke color correctly',()=>{
     let value:string = 'FFFFFF';
     attributeBarComponent.assignStrokeColor(value);
-    expect(attributeBarComponent.shapeStrokeColor).toBe(value);
+    expect(attributeBarComponent.shapeStrokeColor).toEqual(value);
   });
 
   it('Should set FillColor correctly',() => {
     let value: string = '111111';
     attributeBarComponent.assignFillColor(value);
-    expect(attributeBarComponent.shapeFillColor).toBe(value);
+    expect(attributeBarComponent.shapeFillColor).toEqual(value);
   } );
 
   it('Should set BrushStrokeWidth correctly',() => {
     let value:number = 10;
     attributeBarComponent.assignBrushStrokeWidth(value);
-    expect(attributeBarComponent.brushStrokeWidth).toBe(value);
+    expect(attributeBarComponent.brushStrokeWidth).toEqual(value);
   });
 
   it('Should set PenStroleWidth correctly',() => {
     attributeBarComponent.assignPenStrokeWidth(10);
-    expect(attributeBarComponent.penStrokeWidth).toBe(10);
+    expect(attributeBarComponent.penStrokeWidth).toEqual(10);
   });
 
 

baff2815b7646cca4a15ac5da6c1191a33b79c12
Unknown <j-m.lasnier@hotmail.com>
2019-09-28 20:12:24 -0400

    setRectangleType function from shapesService has been tested [JL](T)

--- client/src/app/services/shapes/shapes.service.spec.ts
+++ client/src/app/services/shapes/shapes.service.spec.ts
@@ -1,12 +1,39 @@
+import { TestBed } from '@angular/core/testing';
 import { Pen } from 'src/Classes/Shapes/pen';
 import { Rectangle } from 'src/Classes/Shapes/rectangle';
+import { COLORS } from 'src/constants';
 import { Brush } from './../../../Classes/Shapes/brush';
 import { ShapesService } from './shapes.service';
 
 describe('ShapesService', () => {
   let service: ShapesService;
   beforeEach(() => {
-    service = new ShapesService();
+    TestBed.configureTestingModule({
+      providers: [
+        ShapesService,
+      ]
+    }).compileComponents();
+    service = TestBed.get(ShapesService);
+  });
+
+  it('Should remove filling color if fillEnable is false', () => {
+    service.fillEnable = false;
+    service.strokeEnable = true;
+    service.fillColor = COLORS.blueRGBA;
+    const spy = spyOn(service, 'removeColor').and.callThrough();
+    service.setRectangleType();
+    expect(service.fillColor).toEqual('rgba(0, 0, 255,0)');
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('Should remove stoke color if strokeEnable is false', () => {
+    service.fillEnable = true;
+    service.strokeEnable = false;
+    service.strokeColor = COLORS.blueRGBA;
+    const spy = spyOn(service, 'removeColor').and.callThrough();
+    service.setRectangleType();
+    expect(service.strokeColor).toEqual('rgba(0, 0, 255,0)');
+    expect(spy).toHaveBeenCalled();
   });
 
   it('Should draw Rectangle', () => {

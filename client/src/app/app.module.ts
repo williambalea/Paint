26b54e3ebf8adcc7eb9b1a45aa19a47189a7b630
ines <ines.goddi@polymtl.ca>
2019-10-05 19:06:12 -0400

    added polygon icon to UI and imported drag and drop module [IG](D)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { DrawingSpaceComponent } from './components/drawing-space/drawing-space.
 import { EntryPointComponent } from './components/entry-point/entry-point.component';
 import { NewFileModalwindowComponent } from './components/new-file-modalwindow/new-file-modalwindow.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -50,6 +51,7 @@ import { SideBarComponent } from './components/side-bar/side-bar.component';
     MatInputModule,
     ReactiveFormsModule,
     FormsModule,
+    DragDropModule
   ],
   entryComponents: [
     EntryPointComponent,

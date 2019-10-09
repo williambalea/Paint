c382ab11d80c1ffb8c6302e6c997f505542a1379
Unknown <j-m.lasnier@hotmail.com>
2019-10-09 14:46:25 -0400

    ellipse service generated [JL](D)

---
+++ client/src/app/services/shapes/ellipse.service.ts
@@ -0,0 +1,23 @@
+import { Shape } from './shape';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EllipseService implements Shape{
+
+  constructor() { }
+
+  onMouseDown(): any {
+
+  }
+
+  onMouseMove(): void {
+
+  }
+
+  onMouseUp(): void {
+
+  }
+
+}

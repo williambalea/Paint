1c4532e15be2a31d4e594ecb40cf5bcdbc327f31
amena <amena.bekteshi@polymtl.ca>
2019-10-11 10:16:26 -0400

    Line service working without preview [AB+WB](D)

--- client/src/app/services/shapes/line.service.ts
+++ client/src/app/services/shapes/line.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, Renderer2 } from '@angular/core';
-import { EMPTY_STRING, INIT_MOVE_PEN, NB } from 'src/constants';
+import { EMPTY_STRING, NB } from 'src/constants';
 import { ColorService } from '../color/color.service';
 import { InputService } from '../input.service';
 import { Shape } from './shape';
+import { Point } from '../../../../../common/interface/point';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +14,8 @@ export class LineService implements Shape {
   stroke: string;
   strokeWidth: number;
   active: boolean;
+  positions: Point[];
+  start: boolean;
 
   path: HTMLElement;
 
@@ -21,29 +24,33 @@ export class LineService implements Shape {
               private colorService: ColorService) {
     this.strokeWidth = NB.Seven;
     this.reset();
+    this.positions = [];
+    this.start = true;
   }
 
   reset(): void {
     this.stroke = EMPTY_STRING;
     this.active = false;
   }
-
   onMouseDown(): any {
+    this.positions.push(this.inputService.getMouse());
     this.active = true;
     this.stroke = this.colorService.getFillColor();
     this.path = this.renderer.createElement('path', 'svg');
     this.renderer.setStyle(this.path, 'stroke', this.stroke.toString());
     this.renderer.setStyle(this.path, 'stroke-linecap', 'round');
     this.renderer.setStyle(this.path, 'stroke-linejoin', 'round');
+    this.renderer.setStyle(this.path, 'fill', 'none');
     this.renderer.setStyle(this.path, 'stroke-width', this.strokeWidth.toString());
-    this.linepath = `M${this.inputService.getMouse().x} ${this.inputService.getMouse().y} ${INIT_MOVE_PEN}`;
+    this.draw();
     this.renderer.setAttribute(this.path, 'd', this.linepath);
+    this.start = false;
     return this.path;
   }
 
   onMouseMove(): void {
     if (this.active) {
-      this.draw();
+      // this.draw();
     }
   }
   onMouseUp(): void {
@@ -53,8 +60,10 @@ export class LineService implements Shape {
   }
 
   draw() {
-    this.linepath += `L${this.inputService.getMouse().x} ${this.inputService.getMouse().y} `;
-    this.renderer.setAttribute(this.path, 'd', this.linepath);
-    this.renderer.setStyle(this.path, 'fill', 'none');
+    if (this.start) {
+    this.linepath = `M${this.positions[0].x} ${this.positions[0].y}`;
+    } else {
+      this.linepath += `L${this.positions[this.positions.length - 1].x} ${this.positions[this.positions.length - 1].y}`;
+    }
   }
 }

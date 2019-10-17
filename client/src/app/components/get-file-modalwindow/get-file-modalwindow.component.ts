9f13e1644c20af9661664826e8e874e0025a8557
ines <ines.goddi@polymtl.ca>
2019-10-17 12:42:49 -0400

    taking off console.logs from getfile modal

--- client/src/app/components/get-file-modalwindow/get-file-modalwindow.component.ts
+++ client/src/app/components/get-file-modalwindow/get-file-modalwindow.component.ts
@@ -41,11 +41,7 @@ export class GetFileModalwindowComponent implements OnInit {
 
     this.communicationService.testReturnIndex().subscribe((table: SVGJSON[]) => {
       this.dataTable = table;
-    
-      console.log('no filer', this.dataTable);
-        this.selectMostRecent(this.dataTable);
-    
-     
+      this.selectMostRecent(this.dataTable);
       });
       this.filterActivated = false;
     
@@ -82,9 +78,7 @@ export class GetFileModalwindowComponent implements OnInit {
    addTagToFilter(): void {
      this.filterActivated = true;
      this.tags.push(this.tag);
-     console.log('tags', this.tags);
      this.updateDisplayTable();
-     console.log(this.displayedData);
    }
 
 

b9bbc59bac195ca36c7e25b21285550f89cda43b
Marc-Antoine Boismenu <listeningmk@gmail.com>
2019-10-18 19:07:35 -0400

    Adding proper mock for renderer in grid service [MB](T)

--- client/src/app/services/grid/grid.service.spec.ts
+++ client/src/app/services/grid/grid.service.spec.ts
@@ -1,16 +1,27 @@
+import { Renderer2 } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { GridService } from './grid.service';
 
-describe('GridService', () => {
+class RendererMock {
+  createElement(): void {return; }
+  setStyle(): void {return; }
+  setAttribute(): void {return; }
+  appendChild(): void {return; }
+}
+
+fdescribe('GridService', () => {
   let service: GridService;
+  let rendererMock: Renderer2;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         GridService,
+        { provide: Renderer2, useClass: RendererMock },
       ],
     }).compileComponents();
     service = TestBed.get(GridService);
+    rendererMock = TestBed.get(Renderer2);
   });
 
   it('should be created', () => {
@@ -30,7 +41,7 @@ describe('GridService', () => {
   it('Should set a numeral opacity', () => {
     expect(service.opacity).toEqual(jasmine.any(Number));
     expect(service.opacity).toBeGreaterThanOrEqual(0);
-    expect(service.opacity).toBeLessThanOrEqual(1);
+    expect(service.opacity).toBeLessThanOrEqual(100);
   });
   it('Should set a numeral grid size', () => {
     expect(service.gridSize).toEqual(jasmine.any(Number));
@@ -38,9 +49,18 @@ describe('GridService', () => {
     expect(service.gridSize).toBeLessThanOrEqual(200);
   });
   it('Should build a grid with expected parameters', () => {
+    const createElementSpy = spyOn(rendererMock, 'createElement').and.callThrough();
+    const setAttributeSpy = spyOn(rendererMock, 'setAttribute').and.callThrough();
+    const setStyleSpy = spyOn(rendererMock, 'setStyle').and.callThrough();
+    const appendChildSpy = spyOn(rendererMock, 'appendChild').and.callThrough();
+
     service.gridSize = 50;
     service.draw(service.gridSize);
+    expect(createElementSpy).toHaveBeenCalled();
     expect(service.gridSize).toEqual(50);
-    /* add more logic */
+    expect(setAttributeSpy).toHaveBeenCalled();
+    expect(setStyleSpy).toHaveBeenCalled();
+    expect(appendChildSpy).toHaveBeenCalled();
+
   });
 });

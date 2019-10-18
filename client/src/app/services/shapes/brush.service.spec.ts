65fc28fa3ebaab5581b0745665e01a9070eb67a3
Unknown <j-m.lasnier@hotmail.com>
2019-10-18 14:28:06 -0400

    test bug fixed [JL+AB](T)

--- client/src/app/services/shapes/brush.service.spec.ts
+++ client/src/app/services/shapes/brush.service.spec.ts
@@ -3,7 +3,6 @@ import { TestBed } from '@angular/core/testing';
 import { ColorService } from '../color/color.service';
 import { InputService } from '../input.service';
 import { BrushService } from './brush.service';
-import { Renderer2 } from '@angular/core';
 
 describe('BrushService', () => {
   let service: BrushService;

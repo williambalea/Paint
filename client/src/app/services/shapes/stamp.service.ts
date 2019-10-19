13e2a661d91742ec6fd875b375316255bd8e2d13
Omar BEN SALAH <omar.ben-salah@polymtl.ca>
2019-10-18 21:22:54 -0400

    added common part to svgFileLocation [OBS] (A)

--- client/src/app/services/shapes/stamp.service.ts
+++ client/src/app/services/shapes/stamp.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Renderer2 } from '@angular/core';
-import { svgFileDataBase64, svgFileLocation } from 'src/assets/stamps/svgFileLocaltion';
+import { svgFileDataBase64, svgFileLocation } from 'src/assets/stamps/svgFileLocation';
 import { EMPTY_STRING, NB } from 'src/constants';
 import { Point } from '../../../../../common/interface/point';
 import { InputService } from '../input.service';

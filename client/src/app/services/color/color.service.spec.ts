538b8914ccd91731d6f45812490b49d155d84081
Amena <amena.bekteshi@polymtl.ca>
2019-10-19 19:45:04 -0400

    Add tests for colorService [AB](T)

--- client/src/app/services/color/color.service.spec.ts
+++ client/src/app/services/color/color.service.spec.ts
@@ -6,6 +6,39 @@ describe('ColorService', () => {
     expect(colorService).toBeDefined();
   });
 
+  it('should get backround color', () => {
+    const result = colorService.getBackgroundColor();
+    expect(result).toBeDefined();
+  });
+
+  it('should set backround color', () => {
+    colorService.setBackgroundColor('red');
+    const result = colorService.getBackgroundColor();
+    expect(result).toEqual('red');
+  });
+
+  it('should get Background Button', () => {
+    const result = colorService.getShowBackgroundButton();
+    expect(result).toBeDefined();
+  });
+
+  it('should set Background Button', () => {
+    colorService.setShowBackgroundButton(true);
+    const result = colorService.getShowBackgroundButton();
+    expect(result).toEqual(true);
+  });
+
+  it('should get Used primary', () => {
+    const result = colorService.getUsingPrimary();
+    expect(result).toBeDefined();
+  });
+
+  it('should set Used primary', () => {
+    colorService.setUsingPrimary(true);
+    const result = colorService.getUsingPrimary();
+    expect(result).toEqual(true);
+  });
+
   it('Should manage last ten used colors correctly', () => {
     colorService.addColorsToLastUsed('rgba(240, 240, 240, 1)', 'rbga(80, 80, 80, 1)');
     expect(colorService.verifyIfColorExistsInLastTen('rgba(240, 240, 240, 1)')).toBeTruthy();

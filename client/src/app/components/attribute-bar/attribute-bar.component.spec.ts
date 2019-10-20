7adc47ee5754eebb50a61a5d0fa21d01cb185dac
Unknown <j-m.lasnier@hotmail.com>
2019-10-20 00:46:34 -0400

    attribute bar done with 100 coverage [JL](T)

--- client/src/app/components/attribute-bar/attribute-bar.component.spec.ts
+++ client/src/app/components/attribute-bar/attribute-bar.component.spec.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { MatRadioButton, MatRadioChange } from '@angular/material';
 import { ColorService } from 'src/app/services/color/color.service';
 import { EventEmitterService } from 'src/app/services/event-emitter.service';
+import { BrushService } from 'src/app/services/shapes/brush.service';
+import { LineService } from 'src/app/services/shapes/line.service';
 import { PolygonService } from 'src/app/services/shapes/polygon.service';
 import { RectangleService } from 'src/app/services/shapes/rectangle.service';
-import { EMPTY_STRING } from '../../../constants';
+import { EMPTY_STRING } from 'src/constants';
 import { OUTLINE_TYPE, TOOL } from './../../../constants';
 import { EllipseService } from './../../services/shapes/ellipse.service';
 import { AttributeBarComponent } from './attribute-bar.component';
@@ -26,6 +28,19 @@ export class EllipseServiceMock {
 export class PolygonServiceMock {
   polygonType = EMPTY_STRING;
   assignPolygonType(): void { return; }
+}
+
+// tslint:disable-next-line: max-classes-per-file
+export class BrushServiceMock {
+  changeFilter(): void {return; }
+}
+
+// tslint:disable-next-line: max-classes-per-file
+export class LineServiceMock {
+  junctionStyle = EMPTY_STRING;
+  dashArrayType = EMPTY_STRING;
+  changeJunction(): void {return; }
+  assignStrokeStyle(): void {return; }
 }
 
 // tslint:disable-next-line: max-classes-per-file
@@ -50,6 +65,8 @@ describe('AttributeBarComponent', () => {
   let rectangleService: RectangleService;
   let ellipseService: EllipseService;
   let polygonService: PolygonService;
+  let brushService: BrushService;
+  let lineService: LineService;
   let eventEmitterService: EventEmitterService;
   // let matRadioButton: MatRadioButton;
 
@@ -67,6 +84,8 @@ describe('AttributeBarComponent', () => {
         {provide: RectangleService, useClass: RectangleServiceMock},
         {provide: EllipseService, useClass: EllipseServiceMock},
         {provide: PolygonService, useClass: PolygonServiceMock},
+        {provide: BrushService, useClass: BrushServiceMock},
+        {provide: LineService, useClass: LineServiceMock},
         {provide: Renderer2, useClass: Renderer2Mock },
       ],
       imports: [
@@ -80,6 +99,8 @@ describe('AttributeBarComponent', () => {
     rectangleService = TestBed.get(RectangleService);
     ellipseService = TestBed.get(EllipseService);
     polygonService = TestBed.get(PolygonService);
+    brushService = TestBed.get(BrushService);
+    lineService = TestBed.get(LineService);
     eventEmitterService = TestBed.get(EventEmitterService);
     // matRadioButton = TestBed.get(MatRadioButton);
   }));
@@ -123,7 +144,19 @@ describe('AttributeBarComponent', () => {
     expect(spy).toHaveBeenCalled();
   });
 
-  
+  it('Should not call radio handler on anything if selectedTool doesnt match', () => {
+    const spyPolygon = spyOn(polygonService, 'assignPolygonType');
+    component.selectedTool = TOOL.selector;
+    component.radioChangeHandler({value: OUTLINE_TYPE.bordered} as MatRadioChange);
+    expect(spyPolygon).not.toHaveBeenCalled();
+  });
+
+  it('Should cchange brush filter', () => {
+    const spy = spyOn(brushService, 'changeFilter');
+    component.brushRadioChangeHandler({value: 'abc'} as MatRadioChange);
+    expect(spy).toHaveBeenCalled();
+  });
+
   it('Should call showGrid method from eventEmitterService', () => {
     const showGridSpy = spyOn(eventEmitterService, 'showGrid');
     component.showGrid();
@@ -134,6 +167,56 @@ describe('AttributeBarComponent', () => {
     const hideGridSpy = spyOn(eventEmitterService, 'hideGrid');
     component.hideGrid();
     expect(hideGridSpy).toHaveBeenCalled();
+  });
+
+  it('Should change junction style of lineService', () => {
+    const spy = spyOn(lineService, 'changeJunction');
+    component.lineJunctionChangeHandler({value: 'abc'} as MatRadioChange);
+    expect(lineService.junctionStyle).toEqual('abc');
+    expect(spy).toHaveBeenCalled();
+  });
+  
+
+  it('Should change dashArray style of lineService', () => {
+    const spy = spyOn(lineService, 'assignStrokeStyle');
+    component.lineStypeRadioChangeHandler({value: 'abc'} as MatRadioChange);
+    expect(lineService.dashArrayType).toEqual('abc');
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('Should return ColorService',() => {
+    const result = component.getColorService();
+    expect(result).toBeDefined();
+  });
+
+  it('Should return PenService',() => {
+    const result = component.getPenService();
+    expect(result).toBeDefined();
+  });
+
+  it('Should return brushService',() => {
+    const result = component.getBrushService();
+    expect(result).toBeDefined();
+  });
+  
+  it('Should return LineService',() => {
+    const result = component.getLineService();
+    expect(result).toBeDefined();
+  });
+  
+  it('Should return stampService',() => {
+    const result = component.getStampService();
+    expect(result).toBeDefined();
+  });
+  
+  it('Should return InputService',() => {
+    const result = component.getInputService();
+    expect(result).toBeDefined();
+  });  
+    
+  it('Should return gridService',() => {
+    const result = component.getGridService();
+    expect(result).toBeDefined();
   });
 
 });

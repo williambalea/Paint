9b72b6046628625e95872bc68ed6095059095014
Amena <amena.bekteshi@polymtl.ca>
2019-10-20 00:10:11 -0400

    Create RenderMock and SelectorServiceMock to test IncludingBoxService [AB](T)

--- client/src/app/services/includingBox/including-box.service.spec.ts
+++ client/src/app/services/includingBox/including-box.service.spec.ts
@@ -1,16 +1,38 @@
 import { Renderer2 } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { SelectorService } from '../selector/selector.service';
 import { IncludingBoxService } from './including-box.service';
 
+class RendererMock {
+  createElement(): void {return; }
+  //mettre les fonctions ici
+}
+// tslint:disable-next-line: max-classes-per-file
+class SelectorServiceServiceMock {
+  //mettre les fonctions ici
+}
+
 describe('IncludingBoxService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      Renderer2,
-    ],
-  }));
+  let service: IncludingBoxService;
+  // let renderer: Renderer2;
+  // let selectorService: SelectorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        Renderer2,
+        SelectorService,
+        IncludingBoxService,
+        { provide: Renderer2, useClass: RendererMock },
+        { provide: SelectorService, useClass: SelectorServiceServiceMock },
+      ],
+    }).compileComponents();
+    service = TestBed.get(IncludingBoxService);
+    // renderer = TestBed.get(Renderer2);
+    // selectorService = TestBed.get(SelectorService);
+  });
 
   it('should be created', () => {
-    const service: IncludingBoxService = TestBed.get(IncludingBoxService);
     expect(service).toBeTruthy();
   });
 });

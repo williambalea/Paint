7cbb0f9826b006549fb8fa0237679488d285cac1
Unknown <j-m.lasnier@hotmail.com>
2019-10-20 00:10:41 -0400

    tests added for radiohandler in attributeBarComponent [JL](T)

--- client/src/app/components/attribute-bar/attribute-bar.component.spec.ts
+++ client/src/app/components/attribute-bar/attribute-bar.component.spec.ts
@@ -1,15 +1,31 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Renderer2 } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, ElementRef, Renderer2 } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { MatRadioButton, MatRadioChange } from '@angular/material';
 import { ColorService } from 'src/app/services/color/color.service';
 import { EventEmitterService } from 'src/app/services/event-emitter.service';
+import { PolygonService } from 'src/app/services/shapes/polygon.service';
 import { RectangleService } from 'src/app/services/shapes/rectangle.service';
 import { EMPTY_STRING } from '../../../constants';
+import { OUTLINE_TYPE, TOOL } from './../../../constants';
+import { EllipseService } from './../../services/shapes/ellipse.service';
 import { AttributeBarComponent } from './attribute-bar.component';
 
 export class RectangleServiceMock {
   rectangleType = EMPTY_STRING;
   assignRectangleType(): void { return; }
+}
+
+// tslint:disable-next-line: max-classes-per-file
+export class EllipseServiceMock {
+  ellipseType = EMPTY_STRING;
+  assignEllipseType(): void { return; }
+}
+
+// tslint:disable-next-line: max-classes-per-file
+export class PolygonServiceMock {
+  polygonType = EMPTY_STRING;
+  assignPolygonType(): void { return; }
 }
 
 // tslint:disable-next-line: max-classes-per-file
@@ -22,12 +38,20 @@ export class EventEmitterServiceMock {
   showGrid(): void {return; }
 }
 
+// tslint:disable-next-line: max-classes-per-file
+export class MockElementRef extends ElementRef {
+  constructor() { super(null); }
+}
+
 describe('AttributeBarComponent', () => {
   let component: AttributeBarComponent;
   let fixture: ComponentFixture<AttributeBarComponent>;
   let colorService: ColorService;
-  // let rectangleService: RectangleService;
+  let rectangleService: RectangleService;
+  let ellipseService: EllipseService;
+  let polygonService: PolygonService;
   let eventEmitterService: EventEmitterService;
+  // let matRadioButton: MatRadioButton;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -36,8 +60,13 @@ describe('AttributeBarComponent', () => {
       ],
       providers: [
         AttributeBarComponent,
+        RectangleService,
+        MatRadioButton,
+        {provide: ElementRef, useClass: MockElementRef},
         {provide: eventEmitterService, useClass: EventEmitterServiceMock},
         {provide: RectangleService, useClass: RectangleServiceMock},
+        {provide: EllipseService, useClass: EllipseServiceMock},
+        {provide: PolygonService, useClass: PolygonServiceMock},
         {provide: Renderer2, useClass: Renderer2Mock },
       ],
       imports: [
@@ -48,8 +77,11 @@ describe('AttributeBarComponent', () => {
     .compileComponents();
     component = TestBed.get(AttributeBarComponent);
     colorService = TestBed.get(ColorService);
-    // rectangleService = TestBed.get(RectangleService);
+    rectangleService = TestBed.get(RectangleService);
+    ellipseService = TestBed.get(EllipseService);
+    polygonService = TestBed.get(PolygonService);
     eventEmitterService = TestBed.get(EventEmitterService);
+    // matRadioButton = TestBed.get(MatRadioButton);
   }));
 
   beforeEach(() => {
@@ -67,16 +99,31 @@ describe('AttributeBarComponent', () => {
     expect(colorService).toBeDefined();
   });
 
-  // it('Should call radio handler', () => {
-  //   // let button = fixture.debugElement.nativeElement.querySelector('button');
-  //   const radio = fixture.MatRadioChange.query(By.css('input[value=Bordered]'));
-  //   const spy = spyOn(rectangleService, 'assignRectangleType');
-  //   component.selectedTool = TOOL.rectangle;
-  //   component.radioChangeHandler(radio);
-  //   expect(rectangleService.rectangleType).toEqual(OUTLINE_TYPE.filled);
-  //   expect(spy).toHaveBeenCalled();
-  // });
+  it('Should call radio handler on rectangle', () => {
+    const spy = spyOn(rectangleService, 'assignRectangleType');
+    component.selectedTool = TOOL.rectangle;
+    component.radioChangeHandler({value: OUTLINE_TYPE.bordered} as MatRadioChange);
+    expect(rectangleService.rectangleType).toEqual(OUTLINE_TYPE.bordered);
+    expect(spy).toHaveBeenCalled();
+  });
 
+  it('Should call radio handler on ellipse', () => {
+    const spy = spyOn(ellipseService, 'assignEllipseType');
+    component.selectedTool = TOOL.ellipse;
+    component.radioChangeHandler({value: OUTLINE_TYPE.bordered} as MatRadioChange);
+    expect(ellipseService.ellipseType).toEqual(OUTLINE_TYPE.bordered);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('Should call radio handler on polygon', () => {
+    const spy = spyOn(polygonService, 'assignPolygonType');
+    component.selectedTool = TOOL.polygon;
+    component.radioChangeHandler({value: OUTLINE_TYPE.bordered} as MatRadioChange);
+    expect(polygonService.polygonType).toEqual(OUTLINE_TYPE.bordered);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  
   it('Should call showGrid method from eventEmitterService', () => {
     const showGridSpy = spyOn(eventEmitterService, 'showGrid');
     component.showGrid();

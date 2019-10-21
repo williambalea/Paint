2db405aaeba50161a91121ef2de53994613cb6fb
Unknown <j-m.lasnier@hotmail.com>
2019-10-21 18:52:36 -0400

    added tests to no-shape [JL](T)

--- client/src/app/services/shapes/no-shape.service.spec.ts
+++ client/src/app/services/shapes/no-shape.service.spec.ts
@@ -3,10 +3,34 @@ import { TestBed } from '@angular/core/testing';
 import { NoShapeService } from './no-shape.service';
 
 describe('NoShapeService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: NoShapeService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+  }).compileComponents();
+    service = TestBed.get(NoShapeService);
+});
 
   it('should be created', () => {
-    const service: NoShapeService = TestBed.get(NoShapeService);
     expect(service).toBeTruthy();
   });
+
+  it('should call onMouseDown()', () => {
+    const spy = spyOn(service, 'onMouseDown');
+    service.onMouseDown();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should call onMouseMove()', () => {
+    const spy = spyOn(service, 'onMouseMove');
+    service.onMouseMove();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should call onMouseUp()', () => {
+    const spy = spyOn(service, 'onMouseUp');
+    service.onMouseUp();
+    expect(spy).toHaveBeenCalled();
+  });
+
 });

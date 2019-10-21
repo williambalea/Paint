b25cdfe834330dd33ceb980c6ed2c02d202d4018
Unknown <j-m.lasnier@hotmail.com>
2019-10-21 13:34:49 -0400

    display confirm component tests all done [JL](T)

--- client/src/app/components/display-confirmation/display-confirmation.component.spec.ts
+++ client/src/app/components/display-confirmation/display-confirmation.component.spec.ts
@@ -1,23 +1,33 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {  MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { DisplayConfirmationComponent } from './display-confirmation.component';
 import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { CommunicationsService } from 'src/app/services/communications.service';
+import { EventEmitterService } from './../../services/event-emitter.service';
+import { DisplayConfirmationComponent } from './display-confirmation.component';
+
+class CommunicationsServiceMock {
+  isLoading: boolean;
+}
 
 describe('DisplayConfirmationComponent', () => {
   let component: DisplayConfirmationComponent;
   let fixture: ComponentFixture<DisplayConfirmationComponent>;
+  let communicationsService: CommunicationsService;
+  let eventEmitterService: EventEmitterService;
 
   const mockDialogRef = {
-    close: jasmine.createSpy('close')
+    close: jasmine.createSpy('close'),
   };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ DisplayConfirmationComponent ],
       providers: [
-        MatDialogRef,
+        DisplayConfirmationComponent,
+        EventEmitterService,
         { provide: MatDialogRef, useValue: mockDialogRef },
+        { provide: CommunicationsService, useValue: CommunicationsServiceMock },
       ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
       imports: [
@@ -27,10 +37,13 @@ describe('DisplayConfirmationComponent', () => {
 
     TestBed.overrideModule(BrowserDynamicTestingModule, {
       set: {
-        entryComponents: [DisplayConfirmationComponent]
-      }
+        entryComponents: [DisplayConfirmationComponent],
+      },
     });
     TestBed.compileComponents();
+    component = TestBed.get(DisplayConfirmationComponent);
+    communicationsService = TestBed.get(CommunicationsService);
+    eventEmitterService = TestBed.get(EventEmitterService);
   }));
 
   beforeEach(() => {
@@ -42,4 +55,15 @@ describe('DisplayConfirmationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should confirm', () => {
+    const appendSpy = spyOn(eventEmitterService, 'appendToDrawingSpace');
+
+    component.confirm();
+
+    expect(communicationsService.isLoading).toEqual(true);
+    expect(appendSpy).toHaveBeenCalled();
+
+  });
+
 });

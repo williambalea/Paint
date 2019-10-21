9a05442c69ccc45ee1dfb274513a86cc26f0970b
Amena <amena.bekteshi@polymtl.ca>
2019-10-20 21:22:41 -0400

    Add tests for RectangleService [AB](T)

--- client/src/app/services/shapes/rectangle.service.spec.ts
+++ client/src/app/services/shapes/rectangle.service.spec.ts
@@ -1,3 +1,4 @@
+import { Point } from '@angular/cdk/drag-drop/typings/drag-ref';
 import { Renderer2 } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { ColorService } from '../color/color.service';
@@ -5,27 +6,30 @@ import { InputService } from '../input.service';
 import { RectangleService } from './rectangle.service';
 
 class RendererMock {
-  // createElement(): void {return; }
-  // setStyle(): void {return; }
+  createElement(): void {return; }
+  setStyle(): void {return; }
   // setAttribute(): void {return; }
 }
 // tslint:disable-next-line: max-classes-per-file
 class ColorServiceMock {
   // getFillColor(): void {return; }
   // addColorsToLastUsed(): void {return; }
+  getFillColor(): void {return; }
+  getStrokeColor(): void {return; }
+  createElement(): void {return; }
 }
 
 // tslint:disable-next-line: max-classes-per-file
 class InputServiceMock {
   // backSpacePressed = false;
-  // getMouse(): Point {return {x: 1, y: 2}; }
+  getMouse(): Point {return {x: 1, y: 2}; }
 }
 
 describe('RectangleService', () => {
   let service: RectangleService;
-  // let colorService: ColorService;
+  let colorService: ColorService;
   // let inputService: InputService;
-  // let renderer: Renderer2;
+  let renderer: Renderer2;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -39,12 +43,49 @@ describe('RectangleService', () => {
       ],
     }).compileComponents();
     service = TestBed.get(RectangleService);
-    // colorService = TestBed.get(ColorService);
+    colorService = TestBed.get(ColorService);
     // inputService = TestBed.get(InputService);
-    // renderer = TestBed.get(Renderer2);
+    renderer = TestBed.get(Renderer2);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should reset attributs', () => {
+    service.x = 1;
+    service.y = 2;
+    service.width = 3;
+    service.height = 4;
+    service.active = true;
+    service.reset();
+    expect(service.x).toEqual(0);
+    expect(service.y).toEqual(0);
+    expect(service.width).toEqual(0);
+    expect(service.height).toEqual(0);
+    expect(service.active).toBeFalsy();
+  });
+
+  it ('should excute on mouse down', () => {
+    service.active = false;
+    const spyOnGetFillCollor = spyOn(colorService, 'getFillColor');
+    const spyOnGetStrokeColor = spyOn(colorService, 'getStrokeColor');
+    const spyOnSetOrigin = spyOn(service, 'setOrigin');
+    const spyOnCreateElement = spyOn(renderer, 'createElement');
+    const spyOnSetStyle = spyOn(service, 'setStyle');
+    const spyOnSetRectangleType = spyOn(service, 'setRectangleType');
+    service.onMouseDown();
+    expect(spyOnGetFillCollor).toHaveBeenCalled();
+    expect(spyOnGetStrokeColor).toHaveBeenCalled();
+    expect(spyOnSetOrigin).toHaveBeenCalled();
+    expect(spyOnCreateElement).toHaveBeenCalled();
+    expect(spyOnSetStyle).toHaveBeenCalled();
+    expect(spyOnSetRectangleType).toHaveBeenCalled();
+  });
+
+  it('should set style', () => {
+    const spyOnSetStyle = spyOn(renderer, 'setStyle');
+    service.setStyle();
+    expect(spyOnSetStyle).toHaveBeenCalledTimes(3);
   });
 });

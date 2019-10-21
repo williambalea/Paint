4aee34792944ac8280205b729edc48112e1d48bf
Amena <amena.bekteshi@polymtl.ca>
2019-10-20 20:49:27 -0400

    Adding RendererMock, ColorServiceMock and InputServiceMock for RectangleService's tests [AB](T)

--- client/src/app/services/shapes/rectangle.service.spec.ts
+++ client/src/app/services/shapes/rectangle.service.spec.ts
@@ -1,24 +1,50 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler/src/core';
 import { Renderer2 } from '@angular/core';
-import { async, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { ColorService } from '../color/color.service';
+import { InputService } from '../input.service';
 import { RectangleService } from './rectangle.service';
 
+class RendererMock {
+  // createElement(): void {return; }
+  // setStyle(): void {return; }
+  // setAttribute(): void {return; }
+}
+// tslint:disable-next-line: max-classes-per-file
+class ColorServiceMock {
+  // getFillColor(): void {return; }
+  // addColorsToLastUsed(): void {return; }
+}
+
+// tslint:disable-next-line: max-classes-per-file
+class InputServiceMock {
+  // backSpacePressed = false;
+  // getMouse(): Point {return {x: 1, y: 2}; }
+}
+
 describe('RectangleService', () => {
+  let service: RectangleService;
+  // let colorService: ColorService;
+  // let inputService: InputService;
+  // let renderer: Renderer2;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [],
-      providers: [Renderer2],
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-    })
-    .compileComponents();
-
-  }));
-
-  beforeEach(() => TestBed.configureTestingModule({}));
+      providers: [
+        RectangleService,
+        ColorService,
+        InputService,
+        { provide: Renderer2, useClass: RendererMock },
+        { provide: InputService, useClass: InputServiceMock },
+        { provide: ColorService, useClass: ColorServiceMock },
+      ],
+    }).compileComponents();
+    service = TestBed.get(RectangleService);
+    // colorService = TestBed.get(ColorService);
+    // inputService = TestBed.get(InputService);
+    // renderer = TestBed.get(Renderer2);
+  });
 
   it('should be created', () => {
-    const service: RectangleService = TestBed.get(RectangleService);
     expect(service).toBeTruthy();
   });
 });

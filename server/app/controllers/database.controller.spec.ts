6dfa91e41b311ee4f72ae1323129e3ac271d332e
Omar Ben Salah <ombenm@l4712-02.info.polymtl.ca>
2019-10-22 01:19:59 -0400

    linting server [OBS] (A)

--- server/app/controllers/database.controller.spec.ts
+++ server/app/controllers/database.controller.spec.ts
@@ -3,35 +3,35 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 import { SVGJSON } from '../../../common/communication/SVGJSON';
 import { expect } from 'chai';
-   
 
-describe('databaseController tests', function () {
+describe('databaseController tests', () => () => {
   let assert = require('assert'),
   http = require('http');
   chai.use(chaiHttp);
 
-    it('should be able te retrieve data', function (done) {
-        http.get('http://localhost:3000/database/svgTable', function (res: { on: { (arg0: string, arg1: (chunk: any) => void): void; (arg0: string, arg1: () => void): void; }; }) {
-          let data : any;
-          res.on('data', function (dataFromDatabase: string) {
-            data = dataFromDatabase;
-          });
-          res.on('end',  function () {
-            assert.equal('[]', data);
-            done();
-          });
+  it('should be able te retrieve data', () => (done: any) => {
+      http.get('http://localhost:3000/database/svgTable',
+      () => (res: { on: { (arg0: string, arg1: (chunk: any) => void): void; (arg0: string, arg1: () => void): void; }; }) => {
+        let data: any;
+        res.on('data', () => (dataFromDatabase: string) => {
+          data = dataFromDatabase;
         });
-      });  
+        res.on('end',  () => () => {
+          assert.equal('[]', data);
+          done();
+        });
+      });
+    });
 
-      it('it should POST data in SVGJSON format', (done) => {
-        let data : SVGJSON = {name : 'test', tags : ['test'], thumbnail : 'test', html : 'test', color : 'test'}
-        chai.request('http://localhost:3000/database')
-        .post('/postToTable')
-        .send(data)
-        .end(function (err, res) {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        done();
+  it('it should POST data in SVGJSON format', (done: any) => {
+    let data: SVGJSON = {name : 'test', tags : ['test'], thumbnail : 'test', html : 'test', color : 'test'};
+    chai.request('http://localhost:3000/database')
+    .post('/postToTable')
+    .send(data)
+    .end(() => (err: any, res: any) => {
+    expect(err).to.be.null;
+    expect(res).to.have.status(200);
+    done();
 
   });
 });

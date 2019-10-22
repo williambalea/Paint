f48b08e1ff251ca01e1113a6e3826c88a7313f3f
Unknown <j-m.lasnier@hotmail.com>
2019-10-22 01:28:22 -0400

    tests in eventEmitterService all done with 100 coverage [AB+JL](T)

--- client/src/app/services/event-emitter.service.spec.ts
+++ client/src/app/services/event-emitter.service.spec.ts
@@ -9,4 +9,46 @@ describe('EventEmitterService', () => {
     const service: EventEmitterService = TestBed.get(EventEmitterService);
     expect(service).toBeTruthy();
   });
+
+  it('should emit showgrid', () => {
+    const service: EventEmitterService = TestBed.get(EventEmitterService);
+    const spyEmit = spyOn(service.showGridEmitter, 'emit');
+    service.showGrid();
+    expect(spyEmit).toHaveBeenCalled();
+  });
+
+  it('should emit hideGrid', () => {
+    const service: EventEmitterService = TestBed.get(EventEmitterService);
+    const spyEmit = spyOn(service.hideGridEmitter, 'emit');
+    service.hideGrid();
+    expect(spyEmit).toHaveBeenCalled();
+  });
+
+  it('should emit sendSVGToServer', () => {
+    const service: EventEmitterService = TestBed.get(EventEmitterService);
+    const spyEmit = spyOn(service.sendSVGToServerEmitter, 'emit');
+    service.sendSVGToServer();
+    expect(spyEmit).toHaveBeenCalled();
+  });
+
+  it('should emit appendToDrawingSpace', () => {
+    const service: EventEmitterService = TestBed.get(EventEmitterService);
+    const spyEmit = spyOn(service.appendToDrawingSpaceEmitter, 'emit');
+    service.appendToDrawingSpace();
+    expect(spyEmit).toHaveBeenCalled();
+  });
+
+  it('should emit select', () => {
+    const service: EventEmitterService = TestBed.get(EventEmitterService);
+    const spyEmit = spyOn(service.selectEmitter, 'emit');
+    service.select();
+    expect(spyEmit).toHaveBeenCalled();
+  });
+
+  it('should emit clearCanvas', () => {
+    const service: EventEmitterService = TestBed.get(EventEmitterService);
+    const spyEmit = spyOn(service.clearCanvasEmitter, 'emit');
+    service.clearCanvas();
+    expect(spyEmit).toHaveBeenCalled();
+  });
 });

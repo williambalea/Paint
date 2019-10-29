43d62505dc8f90a8e4337ec9965fcbb296954501
Unknown <j-m.lasnier@hotmail.com>
2019-10-29 19:14:11 -0400

    text line jump possible but still need some little fix

--- client/src/app/services/shapes/text.service.ts
+++ client/src/app/services/shapes/text.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Renderer2 } from '@angular/core';
-import { EMPTY_STRING, NB } from 'src/constants';
+import { EMPTY_STRING, NB, STRINGS } from 'src/constants';
 import { ColorService } from '../color/color.service';
 import { InputService } from '../input.service';
 
@@ -9,12 +9,15 @@ import { InputService } from '../input.service';
 export class TextService {
   x: number;
   y: number;
+  xPos: string;
   font: string;
   fontSize: number;
   text: HTMLElement;
+  tspan: HTMLElement;
   align: string;
   isBold: boolean;
   isItalic: boolean;
+  isEnterPressed: boolean;
   textContent: string;
 
   constructor(private renderer: Renderer2,
@@ -45,9 +48,17 @@ export class TextService {
 
   onMouseDown(): any {
     this.text = this.renderer.createElement('text', 'svg');
+    this.tspan = this.renderer.createElement('tspan', 'svg');
+    this.renderer.appendChild(this.text, this.tspan);
+
     this.textContent = EMPTY_STRING;
-    this.renderer.setAttribute(this.text, 'x', this.inputService.getMouse().x.toString());
+    this.xPos = this.inputService.getMouse().x.toString();
+    this.renderer.setAttribute(this.text, 'x', this.xPos);
     this.renderer.setAttribute(this.text, 'y', (this.inputService.getMouse().y - this.fontSize / 2).toString());
+
+    this.renderer.setAttribute(this.tspan, 'x', this.xPos);
+    this.renderer.setAttribute(this.tspan, 'dy', NB.Zero.toString());
+
     this.update();
     return this.text;
   }
@@ -57,17 +68,29 @@ export class TextService {
     this.renderer.setAttribute(this.text, 'font-size', this.fontSize.toString());
     this.renderer.setAttribute(this.text, 'text-anchor', this.align);
     this.renderer.setAttribute(this.text, 'fill', this.colorService.getFillColor());
-    if (this.isBold) {
-      this.renderer.setAttribute(this.text, 'font-weight', 'bold');
-    } else {
-      this.renderer.setAttribute(this.text, 'font-weight', '');
+    //Bold
+    let boldString: string;
+    this.isBold ? boldString = STRINGS.bold : boldString = EMPTY_STRING;
+    this.renderer.setAttribute(this.text, 'font-weight', boldString);
+    //italic
+    let italicString: string;
+    this.isItalic ? italicString = STRINGS.italic : italicString = EMPTY_STRING;
+    this.renderer.setAttribute(this.text, 'font-style', italicString);
+
+    if(this.inputService.enterPressed) {
+      this.lineJump();
+      this.inputService.enterPressed = false;
     }
-    if (this.isItalic) {
-      this.renderer.setAttribute(this.text, 'font-style', 'italic');
-    } else {
-      this.renderer.setAttribute(this.text, 'font-style', '');
-    }
-    this.renderer.setProperty(this.text, 'innerHTML', this.textContent);
+
+    this.renderer.setProperty(this.tspan, 'innerHTML', this.textContent);
+  }
+
+  lineJump(): void {
+    this.tspan = this.renderer.createElement('tspan', 'svg');
+    this.renderer.setAttribute(this.tspan, 'x', this.xPos);
+    this.renderer.setAttribute(this.tspan, 'dy', this.fontSize.toString());
+    this.renderer.appendChild(this.text, this.tspan);
+    this.textContent = EMPTY_STRING;
   }
 
 }

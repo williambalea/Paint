f5d762acb751fb0a2d97dbf0b38e5f300222a4cf
Amena <amena.bekteshi@polymtl.ca>
2019-10-30 01:47:58 -0400

    Mock colorService, InputService and Renderer to test textService [AB](T)

--- client/src/app/services/shapes/text.service.spec.ts
+++ client/src/app/services/shapes/text.service.spec.ts
@@ -1,12 +1,50 @@
+import { Renderer2 } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-
+import { ColorService } from '../color/color.service';
+import { InputService } from '../input.service';
 import { TextService } from './text.service';
 
-describe('TextService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+class RendererMock {
+  // createElement(): void {return; }
+  // setStyle(): void {return; }
+  // setAttribute(): void {return; }
+}
+// tslint:disable-next-line: max-classes-per-file
+class ColorServiceMock {
+  // getFillColor(): void {return; }
+  // addColorsToLastUsed(): void {return; }
+}
+
+// tslint:disable-next-line: max-classes-per-file
+class InputServiceMock {
+  // backSpacePressed = false;
+  // getMouse(): Point {return {x: 1, y: 2}; }
+}
+
+fdescribe('TextService', () => {
+  let service: TextService;
+  // let colorService: ColorService;
+  // let inputService: InputService;
+  // let renderer: Renderer2;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        TextService,
+        ColorService,
+        InputService,
+        { provide: Renderer2, useClass: RendererMock },
+        { provide: InputService, useClass: InputServiceMock },
+        { provide: ColorService, useClass: ColorServiceMock },
+      ],
+    }).compileComponents();
+    service = TestBed.get(TextService);
+    // colorService = TestBed.get(ColorService);
+    // inputService = TestBed.get(InputService);
+    // renderer = TestBed.get(Renderer2);
+  });
 
   it('should be created', () => {
-    const service: TextService = TestBed.get(TextService);
     expect(service).toBeTruthy();
   });
 });

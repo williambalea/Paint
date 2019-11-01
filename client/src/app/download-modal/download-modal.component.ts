ae2452fa216e191b7eca5723ecb9bcee0c703e8d
Unknown <j-m.lasnier@hotmail.com>
2019-10-31 21:30:51 -0400

    fixed some details in look and code for download locally [AB+JL](D)

--- client/src/app/download-modal/download-modal.component.ts
+++ client/src/app/download-modal/download-modal.component.ts
@@ -6,16 +6,16 @@ import { ExportService } from '../services/export.service';
 @Component({
   selector: 'app-download-modal',
   templateUrl: './download-modal.component.html',
-  styleUrls: ['./download-modal.component.scss']
+  styleUrls: ['./download-modal.component.scss'],
 })
 export class DownloadModalComponent implements OnInit {
-  formats : string[];
-  selectedFormat : string;
-  name : string;
-  fileName : string;
+  formats: string[];
+  selectedFormat: string;
+  name: string;
+  fileName: string;
   fileUrl;
 
-  constructor(private exportService : ExportService,private sanitizer: DomSanitizer, private renderer: Renderer2) {
+  constructor(private exportService: ExportService, private sanitizer: DomSanitizer, private renderer: Renderer2) {
     this.selectedFormat = 'svg';
     this.name = EMPTY_STRING;
     this.fileName = EMPTY_STRING;
@@ -25,22 +25,21 @@ export class DownloadModalComponent implements OnInit {
     console.log(this.exportService.downloadLink);
     const svg = this.renderer.createElement('svg');
     this.renderer.setAttribute(svg, 'viewBox', `0 0 ${this.exportService.canvas.nativeElement.width} ${this.exportService.canvas.nativeElement.height}`);
-    this.renderer.setAttribute(svg,'xmlns', 'http://www.w3.org/2000/svg');
-    this.renderer.appendChild(svg,this.exportService.canvas.nativeElement.cloneNode(true));
-    console.log('svg',svg);
+    this.renderer.setAttribute(svg, 'xmlns', 'http://www.w3.org/2000/svg');
+    this.renderer.appendChild(svg, this.exportService.canvas.nativeElement.cloneNode(true));
+    console.log('svg', svg);
    // console.log(data);
     const blob = new Blob([svg.outerHTML], { type: 'application/octet-stream' });
 
     this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
-   
+
   }
-  click(){
-  
-    if(window.confirm('please confirm export operation')){
-      // if(this.selectedFormat !=='svg')
-      this.exportService.download(this.selectedFormat,this.fileName);
-    }
-   
+  click() {
+
+  //   if (window.confirm('please confirm export operation')) {
+  //     // if(this.selectedFormat !=='svg')
+  //     this.exportService.download(this.selectedFormat, this.fileName);
+  //   }
   }
 
 }

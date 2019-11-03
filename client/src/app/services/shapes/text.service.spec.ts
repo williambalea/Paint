d9fe49e05af35cf18bd5e3130ebf93d0ed552c47
ines <ines.goddi@polymtl.ca>
2019-11-03 11:46:23 -0500

    add test for text service [AB+IG](T)

--- client/src/app/services/shapes/text.service.spec.ts
+++ client/src/app/services/shapes/text.service.spec.ts
@@ -1,46 +1,61 @@
-import { Renderer2, RendererFactory2 } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { Renderer2, RendererFactory2, Type } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { ColorService } from '../color/color.service';
 import { InputService } from '../input.service';
 import { TextService } from './text.service';
+import { DrawingSpaceComponent } from 'src/app/components/drawing-space/drawing-space.component';
+import { SafeHtmlPipe} from '../../safe-html.pipe';
+import { HttpClient,HttpHandler }    from '@angular/common/http';
+
 
 // tslint:disable-next-line: max-classes-per-file
-class ColorServiceMock {
+//class ColorServiceMock {
   // getFillColor(): void {return; }
   // addColorsToLastUsed(): void {return; }
   // getFillColor(): void {return; }
-}
+//}
 
 // tslint:disable-next-line: max-classes-per-file
 class InputServiceMock {
   // backSpacePressed = false;
   // getMouse(): Point {return {x: 1, y: 2}; }
 }
+class Rendere2Mock {
+  createElement(): void {return; }
+}
+
 
 describe('TextService', () => {
   let service: TextService;
-  // let colorService: ColorService;
-  // let inputService: InputService;
   let renderer: Renderer2;
+  let renderer2 : Renderer2;
   let rendererFactory: RendererFactory2;
+  let fixture: ComponentFixture<DrawingSpaceComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      declarations: [DrawingSpaceComponent,SafeHtmlPipe],
       providers: [
         TextService,
         ColorService,
         InputService,
-        // { provide: RendererFactory2, useClass: RendererFactoryMock },
+        Renderer2,
+        HttpClient,
+        HttpHandler,
         { provide: InputService, useClass: InputServiceMock },
-        { provide: ColorService, useClass: ColorServiceMock },
+        { provide: Renderer2, useClass: Rendere2Mock },
+       // { provide: ColorService, useClass: ColorServiceMock },
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(DrawingSpaceComponent);
+   
+    fixture.detectChanges();
     service = TestBed.get(TextService);
-    // colorService = TestBed.get(ColorService);
-    // inputService = TestBed.get(InputService);
-    // renderer = TestBed.get(Renderer2);
+    
     rendererFactory = TestBed.get(RendererFactory2);
     renderer = rendererFactory.createRenderer(null, null);
+    renderer2 = TestBed.get(Renderer2);
+    renderer2 = fixture.componentRef.injector.get<Renderer2>(Renderer2 as Type<Renderer2>);
   });
 
   it('should be created', () => {
@@ -73,23 +88,29 @@ describe('TextService', () => {
 
   it('should create text Elements', () => {
     const spyOnCreateElement = spyOn(renderer, 'createElement');
+     spyOn(renderer2, 'createElement');
+    
     const spyOnAppendChild = spyOn(renderer, 'appendChild');
     service.createTextElements();
     expect(spyOnCreateElement).toHaveBeenCalledTimes(2);
     expect(spyOnAppendChild).toHaveBeenCalledTimes(1);
   });
 
-  // it('should excute on mouse down', () => {
-  //   const spyOnCreateTextElements = spyOn(service, 'createTextElements');
-  //   const spyOnSetTextAttributes = spyOn(service, 'setTextAttributes');
-  //   const spyOnUpdate = spyOn(service, 'update');
-  //   const spyOnAppendChild = spyOn(renderer, 'appendChild');
-  //   service.onMouseDown();
-  //   expect(spyOnCreateTextElements).toHaveBeenCalled();
-  //   expect(spyOnSetTextAttributes).toHaveBeenCalled();
-  //   expect(spyOnUpdate).toHaveBeenCalled();
-  //   expect(spyOnAppendChild).toHaveBeenCalled();
-  // });
+  it('should excute on mouse down', () => {
+    
+    
+    const spyOnCreateTextElements = spyOn(service, 'createTextElements');
+    const spyOnSetTextAttributes = spyOn(service, 'setTextAttributes');
+    const spyOnUpdate = spyOn(service, 'update');
+    const spyOnAppendChild = spyOn(renderer, 'appendChild');
+    service.onMouseDown();
+
+   
+    expect(spyOnCreateTextElements).toHaveBeenCalled();
+    expect(spyOnSetTextAttributes).toHaveBeenCalled();
+    expect(spyOnUpdate).toHaveBeenCalled();
+    expect(spyOnAppendChild).toHaveBeenCalled();
+  });
 
   // it('should excute on mouse down', () => {
   //   const spyOnCreateTextElements = spyOn(service, 'createTextElements');

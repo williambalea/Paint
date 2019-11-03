38a41e034f397b2b26f173e8a6210f1e03ec98f3
ines <ines.goddi@polymtl.ca>
2019-11-03 14:27:24 -0500

    bug fix svg export export background color [IG](D)

--- client/src/app/upload-modal/upload-modal.component.ts
+++ client/src/app/upload-modal/upload-modal.component.ts
@@ -19,19 +19,25 @@ export class UploadModalComponent implements OnInit {
   fileContent: string = '';
  
   public onChange(fileList: FileList): void {
+   
 
     let file = fileList[0];
     let fileReader: FileReader = new FileReader();
     let self = this;
     fileReader.onloadend = (() => {
       self.fileContent = fileReader.result as string;
+   
       this.uploadService.fileContent = self.fileContent.slice(96,-6);
       console.log(this.uploadService.fileContent.slice(0,2));
-      console.log(this.uploadService.fileContent);
-   
+      for (let i = 25; i < 1000; i++) {
+        console.log(this.uploadService.fileContent[i]);
+        this.uploadService.backgroundColor += this.uploadService.fileContent[i];
+        if(this.uploadService.fileContent[i] === ')'){
+          break;
+        }
+      }
+    
     }) 
-   
-  console.log('file',file.type);
   if(file.type === 'image/svg+xml'){
     this.uploadService.enableUploadButton = true;
     fileReader.readAsText(file);

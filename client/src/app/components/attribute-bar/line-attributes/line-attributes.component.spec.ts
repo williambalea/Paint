aff415cb55b5fbd077574da4d3ec6f37903fc3af
Marc-Antoine Boismenu <listeningmk@gmail.com>
2019-11-06 16:17:22 -0500

    Full test coverage in line-attributes [MB](T)

--- client/src/app/components/attribute-bar/line-attributes/line-attributes.component.spec.ts
+++ client/src/app/components/attribute-bar/line-attributes/line-attributes.component.spec.ts
@@ -1,10 +1,13 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LineAttributesComponent } from './line-attributes.component';
+import { LineService } from 'src/app/services/shapes/line.service';
 
 describe('LineAttributesComponent', () => {
   let component: LineAttributesComponent;
   let fixture: ComponentFixture<LineAttributesComponent>;
+  let lineService: LineService;
+  const event: any = { value : 'value' };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,9 +21,23 @@ describe('LineAttributesComponent', () => {
     fixture = TestBed.createComponent(LineAttributesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    lineService = TestBed.get(LineService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change line junction upon radio modification', () => {
+    const changeJunctionSpy = spyOn(lineService, 'changeJunction');
+    component.lineJunctionChangeHandler(event);
+    expect(changeJunctionSpy).toHaveBeenCalled();
+    expect(lineService.junctionStyle).toEqual(event.value);
+  });
+
+  it('should change line style upon radio modification', () => {
+    const assignStrokeStyleSpy = spyOn(lineService, 'assignStrokeStyle');
+    component.lineStypeRadioChangeHandler(event);
+    expect(assignStrokeStyleSpy).toHaveBeenCalled();
   });
 });

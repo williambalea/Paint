d3e37f59604abae1a0e91171c9fe12a197287711
Marc-Antoine Boismenu <listeningmk@gmail.com>
2019-11-06 16:35:56 -0500

    Full test coverage in rectangle-attributes [MB](T)

--- client/src/app/components/attribute-bar/rectangle-attributes/rectangle-attributes.component.spec.ts
+++ client/src/app/components/attribute-bar/rectangle-attributes/rectangle-attributes.component.spec.ts
@@ -1,10 +1,13 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RectangleService } from 'src/app/services/shapes/rectangle.service';
 import { RectangleAttributesComponent } from './rectangle-attributes.component';
 
 describe('RectangleAttributesComponent', () => {
   let component: RectangleAttributesComponent;
   let fixture: ComponentFixture<RectangleAttributesComponent>;
+  let rectangleService: RectangleService;
+  const event: any = { value : 'value' };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,9 +21,17 @@ describe('RectangleAttributesComponent', () => {
     fixture = TestBed.createComponent(RectangleAttributesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    rectangleService = TestBed.get(RectangleService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should assign rectangle type upon radio change', () => {
+    const assignRectangleTypeSpy = spyOn(rectangleService, 'assignRectangleType');
+    component.radioChangeHandler(event);
+    expect(assignRectangleTypeSpy).toHaveBeenCalled();
+    expect(rectangleService.rectangleType).toEqual(event.value);
   });
 });

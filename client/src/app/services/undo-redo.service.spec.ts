ca4eac549828b47a38840c2ef726fc510fca31ee
Amena <amena.bekteshi@polymtl.ca>
2019-11-07 20:24:13 -0500

    Mock services for undo-redo tests [AB](T)

--- client/src/app/services/undo-redo.service.spec.ts
+++ client/src/app/services/undo-redo.service.spec.ts
@@ -1,17 +1,36 @@
-import { TestBed } from '@angular/core/testing';
-
 import { Renderer2 } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { UndoRedoService } from './undo-redo.service';
 
+class RendererMock {
+  createElement(): void {return; }
+  setStyle(): void {return; }
+  setAttribute(): void {return; }
+}
+
 describe('UndoRedoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      Renderer2,
-    ],
-  }));
+  let service: UndoRedoService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        UndoRedoService,
+        // ColorService,
+        // InputService,
+        { provide: Renderer2, useClass: RendererMock },
+        // { provide: InputService, useClass: InputServiceMock },
+        // { provide: ColorService, useClass: ColorServiceMock },
+      ],
+    }).compileComponents();
+    service = TestBed.get(UndoRedoService);
+    // colorService = TestBed.get(ColorService);
+    // inputService = TestBed.get(InputService);
+    // rendererFactory = TestBed.get(RendererFactory2);
+    // renderer = rendererFactory.createRenderer(null, null);
+    });
 
   it('should be created', () => {
-    const service: UndoRedoService = TestBed.get(UndoRedoService);
     expect(service).toBeTruthy();
   });
+
 });

671c0930f25d8855572fcad7279ea7af6867c06b
Amena <amena.bekteshi@polymtl.ca>
2019-11-08 10:23:57 -0500

    Add tests for undo-redo service [AB](T)

--- client/src/app/services/undo-redo.service.spec.ts
+++ client/src/app/services/undo-redo.service.spec.ts
@@ -1,6 +1,8 @@
-import { Renderer2 } from '@angular/core';
+import { Renderer2, RendererFactory2 } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { ACTIONS } from 'src/constants';
 import { UndoRedoService } from './undo-redo.service';
+import { UndoRedoAction } from './undoRedoAction';
 
 class RendererMock {
   createElement(): void {return; }
@@ -10,6 +12,8 @@ class RendererMock {
 
 describe('UndoRedoService', () => {
   let service: UndoRedoService;
+  let rendererFactory: RendererFactory2;
+  let renderer: Renderer2;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -25,12 +29,24 @@ describe('UndoRedoService', () => {
     service = TestBed.get(UndoRedoService);
     // colorService = TestBed.get(ColorService);
     // inputService = TestBed.get(InputService);
-    // rendererFactory = TestBed.get(RendererFactory2);
-    // renderer = rendererFactory.createRenderer(null, null);
+    rendererFactory = TestBed.get(RendererFactory2);
+    renderer = rendererFactory.createRenderer(null, null);
     });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should add action', () => {
+    const spyOnPush = spyOn(service.actions, 'push');
+    const action: UndoRedoAction = { action: ACTIONS.append , shape: SVGGraphicsElement = renderer.createElement('rect', 'svg')};
+    service.addAction(action);
+    expect(spyOnPush).toHaveBeenCalled();
+  });
+
+  // it('append on undo', () => {
+
+  // });
+
 
 });

92367250c5586beb1c7ee7bb17b2b71533e02dc8
Amena <amena.bekteshi@polymtl.ca>
2019-11-09 15:48:57 -0500

    Debug and add tests on undo-redo service [AB](T)

--- client/src/app/services/undo-redo.service.spec.ts
+++ client/src/app/services/undo-redo.service.spec.ts
@@ -1,8 +1,10 @@
-import { Renderer2, RendererFactory2 } from '@angular/core';
+import { Renderer2, RendererFactory2, ElementRef } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { ACTIONS } from 'src/constants';
 import { UndoRedoService } from './undo-redo.service';
 import { UndoRedoAction } from './undoRedoAction';
+import { ViewChildService } from './view-child.service';
+import { InputService } from './input.service';
 
 class RendererMock {
   createElement(): void {return; }
@@ -14,11 +16,15 @@ describe('UndoRedoService', () => {
   let service: UndoRedoService;
   let rendererFactory: RendererFactory2;
   let renderer: Renderer2;
+  let viewChildService: ViewChildService;
+  let inputService: InputService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         UndoRedoService,
+        ViewChildService,
+        InputService,
         // ColorService,
         // InputService,
         { provide: Renderer2, useClass: RendererMock },
@@ -27,6 +33,8 @@ describe('UndoRedoService', () => {
       ],
     }).compileComponents();
     service = TestBed.get(UndoRedoService);
+    viewChildService = TestBed.get(ViewChildService);
+    inputService = TestBed.get(InputService);
     // colorService = TestBed.get(ColorService);
     // inputService = TestBed.get(InputService);
     rendererFactory = TestBed.get(RendererFactory2);
@@ -44,8 +52,31 @@ describe('UndoRedoService', () => {
     expect(spyOnPush).toHaveBeenCalled();
   });
 
-  // it('append on undo', () => {
+  it('should append on undi', () => {
+    viewChildService.canvas = new ElementRef('allo');
+    const lastAction: UndoRedoAction = { action: ACTIONS.append , shape: SVGGraphicsElement = renderer.createElement('rect', 'svg')};
+    const spyOnremoveChild = spyOn(renderer, 'removeChild');
+    const spyOnpush = spyOn(service.poppedActions, 'push');
+    const spyOnvValidateIncrement = spyOn(service, 'validateIncrement');
+    service.appendOnUndo(lastAction);
+    expect(spyOnremoveChild).toHaveBeenCalled();
+    expect(spyOnpush).toHaveBeenCalled();
+    expect(spyOnvValidateIncrement).toHaveBeenCalled();
+  });
 
-  // });
+  it('should validate increment', () => {
+    const lastAction: UndoRedoAction = { action: ACTIONS.append , shape: SVGGraphicsElement = renderer.createElement('rect', 'svg')};
+    service.validateIncrement(lastAction);
+    expect(inputService.incrementMultiplier).toEqual(1);
+  });
 
+  it('should remove on undo', () => {
+    viewChildService.canvas = new ElementRef('allo');
+    const lastAction: UndoRedoAction = { action: ACTIONS.append , shape: SVGGraphicsElement = renderer.createElement('rect', 'svg')};
+    const spyOnappendChild = spyOn(renderer, 'appendChild');
+    const spyOnpush = spyOn(service.poppedActions, 'push');
+    service.removeOnUndo(lastAction);
+    expect(spyOnappendChild).toHaveBeenCalled();
+    expect(spyOnpush).toHaveBeenCalled();
+  });
 });

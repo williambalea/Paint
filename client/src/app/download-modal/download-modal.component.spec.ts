4ae200130cb19cb03d22c35aa5b3306670e889e1
Unknown <j-m.lasnier@hotmail.com>
2019-11-09 16:11:56 -0500

    doenload modal component tested 100 coverage [JL](T)

--- client/src/app/download-modal/download-modal.component.spec.ts
+++ client/src/app/download-modal/download-modal.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ElementRef, Renderer2 } from '@angular/core';
+import { ElementRef, Renderer2, RendererFactory2 } from '@angular/core';
 import { MatDialogModule } from '@angular/material';
 import { provideAutoMock } from 'src/test.helpers.spec';
 import { ExportService } from '../services/export.service';
@@ -12,6 +12,8 @@ describe('DownloadModalComponent', () => {
   let fixture: ComponentFixture<DownloadModalComponent>;
   let viewChildService: ViewChildService;
   let exportService: ExportService;
+  let renderer: Renderer2;
+  let rendererFactory: RendererFactory2;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -30,6 +32,8 @@ describe('DownloadModalComponent', () => {
     viewChildService = TestBed.get(ViewChildService);
     component = TestBed.get(DownloadModalComponent);
     exportService = TestBed.get(ExportService);
+    rendererFactory = TestBed.get(RendererFactory2);
+    renderer = rendererFactory.createRenderer(null, null);
   }));
 
   beforeEach(() => {
@@ -44,5 +48,14 @@ describe('DownloadModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should svg attributes', () => {
+    // tslint:disable-next-line: no-string-literal
+    const spy = spyOn(component['renderer'], 'setAttribute');
+    let element: SVGGraphicsElement;
+    element = renderer.createElement('rect', 'svg');
+    component.setAttributeSVG(element);
+    expect(spy).toHaveBeenCalledTimes(2);
   });
 });
